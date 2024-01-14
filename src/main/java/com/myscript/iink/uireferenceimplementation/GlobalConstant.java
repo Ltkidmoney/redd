bbd44005f3890e93dda1bf3fb6e58f36ba48e366
Ltkidmoney <156555846+Ltkidmoney@users.noreply.github.com>
2024-01-13 23:14:20 -0800

    Add files via upload

---
+++ src/main/java/com/myscript/iink/uireferenceimplementation/GlobalConstant.java
@@ -0,0 +1,9 @@
+package com.myscript.iink.uireferenceimplementation;
+
+import android.graphics.Typeface;
+
+public class GlobalConstant {
+    public static float textSize = 6.0F;
+    public static Typeface textFamily = Typeface.DEFAULT;
+    public static int textColor = 0xff000000;
+}
---
+++ src/main/java/com/myscript/iink/uireferenceimplementation/LayerView.java
@@ -0,0 +1,318 @@
+// Copyright @ MyScript. All rights reserved.
+
+package com.myscript.iink.uireferenceimplementation;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.Rect;
+import android.graphics.Typeface;
+import android.os.Build;
+import android.provider.Settings;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+
+import com.myscript.iink.Editor;
+import com.myscript.iink.IRenderTarget;
+import com.myscript.iink.IRenderTarget.LayerType;
+import com.myscript.iink.Renderer;
+
+import java.util.EnumSet;
+import java.util.Map;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class LayerView extends View
+{
+  private final static int MODEL = 0;
+  private final static int CAPTURE = 1;
+  private ImageLoader imageLoader;
+
+  @Nullable
+  private Map<String, Typeface> typefaceMap;
+
+  @Nullable
+  private Renderer lastRenderer = null;
+
+  @Nullable
+  private OfflineSurfaceManager offlineSurfaceManager = null;
+  @Nullable
+  private Renderer renderer = null;
+  @NonNull
+  private Rect updateArea = new Rect(0, 0, 0, 0);
+  @NonNull
+  private Rect localUpdateArea = new Rect(0, 0, 0, 0);
+  @Nullable
+  private Bitmap bitmap = null; // for API < 28
+  @Nullable
+  private android.graphics.Canvas sysCanvas = null; // for API < 28
+  @Nullable
+  private Canvas iinkCanvas = null;
+
+  private int pageWidth = 0;
+  private int pageHeight = 0;
+  private int viewWidth = 0;
+  private int viewHeight = 0;
+  private int canvasWidth = 0;
+  private int canvasHeight = 0;
+  private int xMin = 0;
+  private int yMin = 0;
+
+  private int imageType = 1;
+
+  public LayerView(Context context)
+  {
+    this(context, null, 0);
+  }
+
+  public LayerView(Context context, @Nullable AttributeSet attrs)
+  {
+    this(context, attrs, 0);
+  }
+
+  public LayerView(Context context, @Nullable AttributeSet attrs, int defStyleAttr)
+  {
+    super(context, attrs, defStyleAttr);
+  }
+
+  public void setRenderTarget(IRenderTarget renderTarget)
+  {
+    // do not need the renderTarget
+  }
+
+  public void setOfflineSurfaceManager(@Nullable OfflineSurfaceManager offlineSurfaceManager)
+  {
+    this.offlineSurfaceManager = offlineSurfaceManager;
+  }
+
+  public void setEditor(Editor editor)
+  {
+    // do not need the editor
+  }
+
+  public void setImageLoader(ImageLoader imageLoader)
+  {
+    this.imageLoader = imageLoader;
+  }
+
+  public void setCustomTypefaces(Map<String, Typeface> typefaceMap)
+  {
+    this.typefaceMap = typefaceMap;
+  }
+
+  @Override
+  protected final void onDraw(android.graphics.Canvas canvas)
+  {
+    super.onDraw(canvas);
+
+    // Draw directly in hardware-accelerated Canvas if scaling is supported (since API 28)
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P)
+    {
+      Renderer renderer;
+      synchronized (this)
+      {
+        localUpdateArea.set(0, 0, canvasWidth, canvasHeight);
+        renderer = lastRenderer;
+      }
+
+      iinkCanvas.setCanvas(canvas);
+
+      prepare(canvas, localUpdateArea);
+
+      Bitmap bg = BitmapFactory.decodeResource(getResources(), imageType == 1 ? R.drawable.image_1 : R.drawable.image_2);
+//      Log.d("CANVAS WIDTH", String.valueOf(canvasWidth));
+//      Log.d("CANVAS HEIGHT", String.valueOf(canvasHeight));
+      canvas.drawBitmap(bg, new Rect(0, 0, bg.getWidth(), bg.getHeight()), new Rect(0, 0, canvasWidth, canvasHeight), null);
+
+      try
+      {
+        renderer.drawModel(localUpdateArea.left, localUpdateArea.top, localUpdateArea.width(), localUpdateArea.height(), iinkCanvas);
+        renderer.drawCaptureStrokes(localUpdateArea.left, localUpdateArea.top, localUpdateArea.width(), localUpdateArea.height(), iinkCanvas);
+      }
+      finally {
+        restore(canvas);
+      }
+
+//      canvas.drawBitmap(bitmap, 0, 0, null);
+    }
+    else // Draw in intermediate bitmap
+    {
+      Renderer renderer;
+      synchronized (this)
+      {
+        localUpdateArea.set(this.updateArea);
+        this.updateArea.setEmpty();
+
+        renderer = lastRenderer;
+        lastRenderer = null;
+      }
+
+      if (!localUpdateArea.isEmpty())
+      {
+        prepare(sysCanvas, localUpdateArea);
+        try
+        {
+          renderer.drawModel(localUpdateArea.left, localUpdateArea.top, localUpdateArea.width(), localUpdateArea.height(), iinkCanvas);
+          renderer.drawCaptureStrokes(localUpdateArea.left, localUpdateArea.top, localUpdateArea.width(), localUpdateArea.height(), iinkCanvas);
+        }
+        finally
+        {
+          restore(sysCanvas);
+        }
+      }
+
+      canvas.drawBitmap(bitmap, 0, 0, null);
+    }
+  }
+
+  @Override
+  protected void onSizeChanged(int newWidth, int newHeight, int oldWidth, int oldHeight)
+  {
+    DisplayMetrics metrics = getContext().getResources().getDisplayMetrics();
+
+    synchronized (this)
+    {
+      // Direct draw
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P)
+      {
+        iinkCanvas = new Canvas(null, typefaceMap, imageLoader, offlineSurfaceManager, metrics.xdpi, metrics.ydpi);
+      }
+      else // Bitmap draw
+      {
+        if (bitmap != null)
+          bitmap.recycle();
+
+        bitmap = Bitmap.createBitmap(newWidth, newHeight, Bitmap.Config.ARGB_8888);
+        sysCanvas = new android.graphics.Canvas(bitmap);
+        iinkCanvas = new Canvas(sysCanvas, typefaceMap, imageLoader, offlineSurfaceManager, metrics.xdpi, metrics.ydpi);
+      }
+
+      iinkCanvas.setClearOnStartDraw(false);
+      canvasWidth = newWidth;
+      canvasHeight = newHeight;
+    }
+
+    super.onSizeChanged(newWidth, newHeight, oldWidth, oldHeight);
+  }
+
+  private void prepare(android.graphics.Canvas canvas, Rect clipRect)
+  {
+    canvas.save();
+    canvas.clipRect(clipRect);
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.P)
+      canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+  }
+
+  private void restore(android.graphics.Canvas canvas)
+  {
+    canvas.restore();
+  }
+
+  public final void update(Renderer renderer, int x, int y, int width, int height, EnumSet<LayerType> layers)
+  {
+    boolean emptyArea;
+
+    // Direct draw
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P)
+    {
+      Rect updatedArea = new Rect(x, y, x + width, y + height);
+      synchronized (this)
+      {
+        if (canvasWidth > 0 && canvasHeight > 0)
+          updatedArea.intersect(new Rect(0, 0, canvasWidth, canvasHeight));
+
+        emptyArea = updatedArea.isEmpty();
+        lastRenderer = renderer;
+      }
+    }
+    else // Bitmap draw
+    {
+      synchronized (this)
+      {
+        updateArea.union(x, y, x + width, y + height);
+        if (canvasWidth > 0 && canvasHeight > 0)
+          updateArea.intersect(new Rect(0, 0, canvasWidth, canvasHeight));
+
+        emptyArea = updateArea.isEmpty();
+        lastRenderer = renderer;
+      }
+    }
+
+    if (!emptyArea)
+    {
+      postInvalidate(x, y, x + width, y + height);
+    }
+  }
+
+  public void setScrollbar(Renderer renderer, int viewWidthPx, int pageWidthPx, int xMin, int viewHeightPx, int pageHeightPx, int yMin)
+  {
+    this.viewWidth = viewWidthPx;
+    this.pageWidth = pageWidthPx;
+    this.renderer = renderer;
+    this.pageHeight = pageHeightPx;
+    this.viewHeight = viewHeightPx;
+    this.xMin = xMin;
+    this.yMin = yMin;
+    setVerticalScrollBarEnabled(true);
+    awakenScrollBars();
+  }
+
+  @Override
+  protected int computeVerticalScrollRange()
+  {
+    return pageHeight;
+  }
+
+  @Override
+  protected int computeVerticalScrollExtent()
+  {
+    return viewHeight;
+  }
+
+  @Override
+  protected int computeVerticalScrollOffset()
+  {
+    return renderer != null ? (int) renderer.getViewOffset().y - yMin : 0;
+  }
+
+  @Override
+  protected int computeHorizontalScrollRange()
+  {
+    return pageWidth;
+  }
+
+  @Override
+  protected int computeHorizontalScrollExtent()
+  {
+    return viewWidth;
+  }
+
+  @Override
+  protected int computeHorizontalScrollOffset()
+  {
+    return renderer != null ? (int) renderer.getViewOffset().x - xMin : 0;
+  }
+
+  public void setBackImage(int number) {
+    imageType = number;
+    invalidate();
+  }
+
+  public void setTextFont(Typeface font) {
+    Log.d("SET TEXT FONT", font.toString());
+    GlobalConstant.textFamily = font;
+  }
+  public void setTextSize(float size) {
+    Log.d("SET TEXT SIZE", String.valueOf(size));
+    GlobalConstant.textSize = size;
+  }
+  public void setTextColor(int textColor) {
+    Log.d("SET TEXT COLOR", String.valueOf(textColor));
+    GlobalConstant.textColor = textColor;
+  }
+}
